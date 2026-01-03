16c2d311e4b254ca821b0f97e93df5316e93e9b1
dakaf <dakafcs@gmail.com>
2026-01-03 08:06:14 -0800

    Use Render PORT

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 /* ================= MIDDLEWARE ================= */
 app.use(cors());
